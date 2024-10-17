712c17d606c027ef1156b98de6e8c132725bebd7
nikhilshah85 <nikhilshah.net@hotmail.com>
2024-10-17 15:25:05 +0530

    This is a very first commit

---
+++ myapp2devops/myapp2devops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myapp2devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
